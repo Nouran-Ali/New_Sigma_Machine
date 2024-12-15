24521ebde58f65c37df24fe54ba161e5f7f9c5d3
Nouran-Ali <noranali237@gmail.com>
2024-12-15 21:58:37 +0200

    edit

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-        <Script
+      <Head>
+        <script
           src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
           type="module"
         />
-
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
       </body>
     </Html>
-  )
+  );
 }
